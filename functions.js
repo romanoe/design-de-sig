43131925f0fb9e61b10f024ffa883b7fad4f6f38
romanoe <noemi.romano@epfl.ch>
2017-11-07 20:23:13 +0100

    Add vector layers as geojson

--- functions.js
+++ functions.js
@@ -1,16 +1,34 @@
-  
+
+
   function initMap() {
+    var limitesAdm = new ol.layer.Vector({
+            source: new ol.source.Vector({
+              url: 'geojson/burkina_faso_administrative.geojson',
+              format: new ol.format.GeoJSON()
+            })
+          });
+
+        var routes = new ol.layer.Vector({
+                  source: new ol.source.Vector({
+                    url: 'geojson/burkina_faso_roads.geojson',
+                    format: new ol.format.GeoJSON(),
+                    fill: 'red'
+                  })
+                });
+
       var map = new ol.Map({
         target: 'map',
         layers: [
           new ol.layer.Tile({
             source: new ol.source.OSM()
-          })
+          }),
+
+          limitesAdm,
+          routes
         ],
         view: new ol.View({
-          center: ol.proj.fromLonLat([37.41, 8.82]),
-          zoom: 4
+          center: ol.proj.fromLonLat([-1.5338800, 12.3656600]),
+          zoom: 7
         })
       });
     }
-    
